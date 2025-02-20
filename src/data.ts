87fc97459f467ee274292ab5635a52046053a406
Rebecka94 <rebeckalarsson.1994@gmail.com>
2025-02-20 12:09:04 +0100

    Added a pages file with three diffrent pages.tsx, updated main.tsx with browserroute paths to the pages and also added a data.ts file with some mocked data

---
+++ src/data.ts
@@ -0,0 +1,27 @@
+export const skidorter = [
+  { id: 1, 
+    namn: "Chamonix", 
+    kontinent: "Europa", 
+    land: "Frankrike" 
+  },
+  { id: 2, 
+    namn: "Aspen", 
+    kontinent: "Nordamerika", 
+    land: "USA" 
+  },
+  { id: 3, 
+    namn: "Niseko", 
+    kontinent: "Asien", 
+    land: "Japan" 
+  },
+  { id: 4, 
+    namn: "Verbier", 
+    kontinent: "Europa", 
+    land: "Schweiz" 
+  },
+  { id: 5, 
+    namn: "Whistler", 
+    kontinent: "Nordamerika", 
+    land: "Kanada" 
+},
+];
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,21 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter, Route, Routes } from "react-router";
+import App from "./App.tsx";
+import AboutPage from "./pages/AboutPage.tsx";
+import HomePage from "./pages/HomePage.tsx";
+import SkiResortDetail from "./pages/SkiResortDetail.tsx";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />}>
+          <Route index element={<HomePage />} />
+          <Route path="about" element={<AboutPage />} />
+        </Route>
+        <Route path="skiresorts" element={<SkiResortDetail />} /> {/* Egen layout */}
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>
+);
